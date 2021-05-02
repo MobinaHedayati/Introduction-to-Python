65a2be96c76a74fd60e8519b62c4d1248c37847e
MobinaHedayati <79896156+MobinaHedayati@users.noreply.github.com>
2021-05-03 02:29:15 +0430

    Delete ex2 directory

--- ex2/1&2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-even = []
-if (num1 % 2) == 0:
-    even.append(num1)
-if (num2 % 2) == 0:
-    even.append(num2)
-print(even)
